64bc06d98fec0cee25d61de6b89e06a13bf1c9b7
rvsfrebeca <rebecaferreiraprofissional@gmail.com>
2021-12-08 01:34:06 -0300

    remover produto

--- back-end/controladores/produtos/remover.js
+++ back-end/controladores/produtos/remover.js
@@ -1,8 +1,17 @@
 const db = require('../../db')
 async function remover(req, res) {
-    await db.collection('produtos').doc(req.params.id).delete();
 
-    return res.json({ mensagem: "Produto excluido com sucesso" })
+    const produtos = db.collection('produtos').doc(req.params.id);
+    const doc = await produtos.get();
+
+    if (!doc.exists) {
+        return res.status(404).json({ mensagem: "Produto não encontrado" })
+    } else {
+        await db.collection('produtos').doc(req.params.id).delete();
+        return res.json({ mensagem: "Produto excluido com sucesso" })
+    }
+
+
 
 
 }
